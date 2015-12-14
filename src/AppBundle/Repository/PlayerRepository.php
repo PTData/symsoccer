db5f8cae0243571f34a50e29e84484e1a2b62b7a
Pedro <ptdata@gmail.com>
2015-12-14 00:18:27 +0000

    Players Organize
    Players Organize in team by position

--- src/AppBundle/Repository/PlayerRepository.php
+++ src/AppBundle/Repository/PlayerRepository.php
@@ -3,15 +3,32 @@
 namespace AppBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 use AppBundle\Entity\Player;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 class PlayerRepository extends EntityRepository {
 
-    public function findTeam($team) {
+    public function findTeamObject($t) {
+        
         $p = $this->getEntityManager()
             ->createQuery(
-                'SELECT p FROM AppBundle:Player p WHERE p.id_team_player = :team '
-            )->setParameter('team', $team)
+                "SELECT p FROM AppBundle:Player p WHERE p.id_team_player = :team  "
+            )->setParameter('team', $t)
             ->getResult();
-            return $p;
+       
+         return $p;
+         
+    }
+    public function findTimeArray($t) {
+        $query = "SELECT * 
+                     FROM player WHERE id_team_player  = :t
+            order by field(situation, 1, 2,0) ,  
+            field(position_player, 'GR', 'DF', 'MD', 'AV')";
+        $q = "select * from player where id_team_player = :t";
+        $stmt = $this->getEntityManager()
+                   ->getConnection()
+                   ->prepare($query);
+      $stmt->bindValue('t', $t);
+      $stmt->execute();
+      return $stmt->fetchAll();
     }
 }
